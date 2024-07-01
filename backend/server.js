59c3a8365dd32a9453b41c7a83690cf0e3ebdd20
Aman Nagar <122695931+aman13nagar@users.noreply.github.com>
2024-07-02 01:38:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,9 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/quizzes', quizRoutes);
 app.use('/api',resultRoutes);
+app.get('/',async(req,res)=>{
+  res.send('server is running')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
